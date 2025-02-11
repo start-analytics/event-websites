df5b962f3e941900553a30f59451fd479fce157e
reidizhupa <reidizhupa56@gmail.com>
2025-02-12 03:10:36 +0900

    metadata generation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from "react";
 import { notFound } from "next/navigation";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { LOCALES, SITE_URL, DEFAULT_LOCALE } from "@config/const";
 
 type Props = {
 	children: ReactNode;
@@ -17,7 +18,39 @@ export async function generateMetadata({ params }: Props) {
 	const t = await getTranslations({ locale: locale, namespace: "Metadata" });
 
 	return {
+		metadataBase: new URL(SITE_URL),
 		title: t("title"),
+		description: t("description"),
+		alternates: {
+			canonical: `${SITE_URL}${locale === DEFAULT_LOCALE ? "" : "/" + locale}`,
+			languages: {
+				...Object.fromEntries(LOCALES.map((l) => [l, `${SITE_URL}${l === DEFAULT_LOCALE ? "" : "/" + l}`])),
+				"x-default": `${SITE_URL}`,
+			},
+		},
+		openGraph: {
+			title: t("title"),
+			description: t("description"),
+			images: [
+				{
+					url: "/android-chrome-192x192.png",
+					width: 800,
+					height: 600,
+					alt: t("title"),
+				},
+			],
+			type: "website",
+		},
+		robots: {
+			index: true,
+			follow: true,
+		},
+		icons: {
+			icon: ["/favicon.ico?v=5"],
+			apple: ["/apple-touch-icon.png?v=5"],
+			shortcut: ["/apple-touch-icon.png"],
+		},
+		manifest: "/site.webmanifest",
 	};
 }
 
